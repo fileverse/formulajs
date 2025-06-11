e4f37dae4d5c536b4d860778c89935028b93a933
0xdeval <26190360+0xdeval@users.noreply.github.com>
2025-06-11 14:51:34 +0100

    Added Blockscout support

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,12 @@
     gnosis: 100,
     base: 8453,
   };
+
+  export const BLOCKSCOUT_CHAINS_MAP = {
+  ethereum: 'https://eth.blockscout.com',
+  gnosis: 'https://gnosis.blockscout.com',
+  arbitrum: 'https://arbitrum.blockscout.com',
+  optimism: 'https://optimism.blockscout.com',
+  soneium: 'https://soneium.blockscout.com',
+  unichain: 'https://unichain.blockscout.com'
+}
