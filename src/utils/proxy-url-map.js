c7bf0f61e837523868cf5d54fb4fb7acecbe0784
Mritunjay Goutam <mritunjaygoutam2204@gmail.com>
2025-07-07 21:34:04 +0530

    minor changes

--- src/utils/proxy-url-map.js
+++ src/utils/proxy-url-map.js
@@ -1,43 +1,44 @@
 import { SERVICES_API_KEY } from '../crypto-constants.js';
+import { MissingApiKeyError } from './error-instances.js'
 
-const stagingFileverseProxyUrl = "https://staging-api-proxy-ca4268d7d581.herokuapp.com/proxy"
-
+const stagingFileverseProxyUrl = "https://staging-api-proxy-ca4268d7d581.herokuapp.com/proxy";
+const productionFileverseProxyUrl = `${process.env.NEXT_PUBLIC_PROXY_BASE_URL}/proxy`
 // Proxy map configuration
 const PROXY_MAP = {
     Etherscan: {
-        url: process.env.NEXT_PUBLIC_PROXY_BASE_URL || stagingFileverseProxyUrl,
+        url: productionFileverseProxyUrl || stagingFileverseProxyUrl,
         removeParams: ['apikey']
     },
     Basescan: {
-        url: process.env.NEXT_PUBLIC_PROXY_BASE_URL || stagingFileverseProxyUrl,
+        url: productionFileverseProxyUrl || stagingFileverseProxyUrl,
         removeParams: ['apikey']
     },
     Gnosisscan: {
-        url: process.env.NEXT_PUBLIC_PROXY_BASE_URL || stagingFileverseProxyUrl,
+        url: productionFileverseProxyUrl || stagingFileverseProxyUrl,
         removeParams: ['apikey']
     },
     Coingecko: {
-        url: process.env.NEXT_PUBLIC_PROXY_BASE_URL || stagingFileverseProxyUrl,
+        url: productionFileverseProxyUrl || stagingFileverseProxyUrl,
         removeParams: ['apikey']
     },
     Firefly: {
-        url: process.env.NEXT_PUBLIC_PROXY_BASE_URL || stagingFileverseProxyUrl,
+        url: productionFileverseProxyUrl || stagingFileverseProxyUrl,
         removeParams: ['apikey']
     },
     Neynar: {
-        url: process.env.NEXT_PUBLIC_PROXY_BASE_URL || stagingFileverseProxyUrl,
+        url: productionFileverseProxyUrl || stagingFileverseProxyUrl,
         removeParams: ['api_key']
     },
     Safe: {
-        url: process.env.NEXT_PUBLIC_PROXY_BASE_URL || stagingFileverseProxyUrl,
+        url: productionFileverseProxyUrl || stagingFileverseProxyUrl,
         removeParams: ['api_key']
     },
     Defillama: {
-        url: process.env.NEXT_PUBLIC_PROXY_BASE_URL || stagingFileverseProxyUrl,
+        url: productionFileverseProxyUrl || stagingFileverseProxyUrl,
         removeParams: ['api_key']
     },
     GnosisPay: {
-        url: process.env.NEXT_PUBLIC_PROXY_BASE_URL || stagingFileverseProxyUrl,
+        url: productionFileverseProxyUrl || stagingFileverseProxyUrl,
         removeParams: ['api_key']
     },
     // Add more services as needed. It can be direct url instead of ENV variable
@@ -76,6 +77,9 @@ function removeUrlParams(url, paramsToRemove) {
 export function getUrlAndHeaders({ url, serviceName, headers = {} }) {
     // Check if proxy is enabled in localStorage
     const apiKeyLS = window.localStorage.getItem(SERVICES_API_KEY[serviceName]);
+    if(SERVICES_API_KEY[serviceName] && (!apiKeyLS || apiKeyLS === '')) {
+        throw new MissingApiKeyError(SERVICES_API_KEY[serviceName])
+    }
     const isProxyModeEnabledValue = apiKeyLS === 'DEFAULT_PROXY_MODE';
 
     // Check if proxy URL exists for this service
