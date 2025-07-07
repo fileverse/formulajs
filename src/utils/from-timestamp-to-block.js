fd4498bc304c71069dec0526da70370126f5c965
Mritunjay Goutam <mritunjaygoutam2204@gmail.com>
2025-07-07 23:29:10 +0530

    minor changes

--- src/utils/from-timestamp-to-block.js
+++ src/utils/from-timestamp-to-block.js
@@ -2,7 +2,7 @@ import { CHAIN_ID_MAP } from './constants.js'
 import { getUrlAndHeaders } from './proxy-url-map.js'
 
 const fromTimeStampToBlock = async (timestamp, chain, apiKey) => {
-  if (!timestamp || !chain || !apiKey) return
+  if (!timestamp || !chain) return
   const chainId = CHAIN_ID_MAP[chain];
   const url = `https://api.etherscan.io/v2/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before&apikey=${apiKey}&chainId=${chainId}`;
   const { URL: finalUrl, HEADERS } = getUrlAndHeaders({ url, serviceName: 'Etherscan', headers: {} });
