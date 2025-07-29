9058e9f3f0880919bebf69704f50f3235184da00
Nadeem <nadeem@fileverse.io>
2025-07-29 19:07:40 +0530

    update scr formula handler

--- test/crypto/smart-contract.test.js
+++ test/crypto/smart-contract.test.js
@@ -1,22 +1,13 @@
 import { expect } from 'chai'
-import { ERROR_MESSAGES_FLAG } from '../../src/utils/constants.js'
 
-import {SMARTCONTRACT} from '../../src/crypto.js'
-
+import { SMARTCONTRACT } from '../../src/crypto.js'
 
 describe('SMARTCONTRACT', () => {
-    it('should return contractName, functionName and args', () => {
-        const result = SMARTCONTRACT('MyContract', 'myFunc', 1, 2, 3, 4)
-        expect(result).to.deep.equal({
-            contractName: 'MyContract',
-            functionName: 'myFunc',
-            args: [1, 2, 3, 4],
-            responseType: 'smart-contract'
-        })
+  it('should return contractName, functionName and args', () => {
+    const result = SMARTCONTRACT('MyContract', 'myFunc', 1, 2, 3, 4)
+    expect(result).to.deep.equal({
+      callSignature: ['MyContract', 'myFunc', 1, 2, 3, 4],
+      responseType: 'smart-contract'
     })
-    it('should throw error when contractName or functionName is invalid', () => {
-        const result = SMARTCONTRACT('MyContract', 9, 1, 2, 3, 4)
-        expect(result.type).to.equal(ERROR_MESSAGES_FLAG.INVALID_PARAM)
-        expect(result.functionName).to.equal('SMARTCONTRACT')
-    })
+  })
 })
