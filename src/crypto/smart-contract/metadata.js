969c829f05ff0e7cf85a481f2bb16f814a22cd65
Joshua Onwuzu <joshuaonwuzu75@gmail.com>
2025-07-30 18:00:42 +0100

    updated SC metadata

--- src/crypto/smart-contract/metadata.js
+++ src/crypto/smart-contract/metadata.js
@@ -1,27 +1,27 @@
 export const SMARTCONTRACT_metadata = {
     n: 'SMARTCONTRACT',
     t: 20,
-    d: 'Pulls data from a given contract',
+    d: 'Query smart contracts in cells.',
     r: 'Call a read-only function on a given smart contract',
     p: [
         {
             name: 'contractName',
             detail: 'Name of the contract you want to pull data from',
-            example: '"My Fileverse portal"',
+            example: '"USDC"',
             require: 'm',
             type: 'string'
         },
         {
             name: 'functionName',
             detail: 'Name of the function you want to call from the give contract',
-            example: '"getCollaborators"',
+            example: '"balanceOf"',
             require: 'm',
             type: 'string'
         },
         {
             name: '...functionArgs',
             detail: 'Optional arguments to pass to the contract function.',
-            example: '1, "0xabc...", true',
+            example: '"0x50Aa3435E310d5a2d15a989Bc353ce7f5682E1d4"',
             require: 'o',
             type: 'any'
         }
