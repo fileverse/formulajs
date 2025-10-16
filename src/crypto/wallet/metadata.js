cc4fdb1f33d98d31ab411e77899cf639b99d1924
Joshua Onwuzu <joshuaonwuzu75@gmail.com>
2025-10-16 07:37:10 +0100

    released @fileverse-dev/formulajs@4.4.27

--- src/crypto/wallet/metadata.js
+++ src/crypto/wallet/metadata.js
@@ -27,7 +27,7 @@ export const WALLET_metadata = {
         },
         {
             name: 'timeframe',
-            detail: 'Comma separated timeframe in hours. Only applies for "txns" queries, if not provided it fetches transaction from latest block.',
+            detail: 'Comma separated timeframe in hours. Only applies to "txns" queries, if not provided it fetches transaction from latest block.',
             example: '"17520"',
             require: 'm',
             type: 'string'
