b946a99a17933645b916bb8766f7c6671454ff41
Maitra Khatri <maitrakhatri@gmail.com>
2025-11-05 13:07:53 +0530

    updated test

--- test/crypto/circles.test.js
+++ test/crypto/circles.test.js
@@ -23,13 +23,13 @@ describe('CIRCLES', () => {
   })
 
   it('should return INVALID_PARAM when address is missing', async () => {
-    const res = await CIRCLES(undefined, 'trust')
+    const res = await CIRCLES('trust', undefined)
     expect(res.type).to.equal(ERROR_MESSAGES_FLAG.INVALID_PARAM)
     expect(res.functionName).to.equal('CIRCLES')
   })
 
   it('should return INVALID_PARAM when functionName is missing', async () => {
-    const res = await CIRCLES('0xe9A6378d8FD4983C2999DB0735f258397E8C2253')
+    const res = await CIRCLES('', '0xe9A6378d8FD4983C2999DB0735f258397E8C2253')
     expect(res.type).to.equal(ERROR_MESSAGES_FLAG.INVALID_PARAM)
     expect(res.functionName).to.equal('CIRCLES')
   })
@@ -38,13 +38,13 @@ describe('CIRCLES', () => {
     sinon
       .stub(fromEnsNameToAddressUtil.default, 'validateAndGetAddress')
       .resolves('0xe9A6378d8FD4983C2999DB0735f258397E8C2253')
-    const res = await CIRCLES('0xe9A6378d8FD4983C2999DB0735f258397E8C2253', 'invalid')
+    const res = await CIRCLES('invalid', '0xe9A6378d8FD4983C2999DB0735f258397E8C2253')
     expect(res.type).to.equal(ERROR_MESSAGES_FLAG.INVALID_PARAM)
     expect(res.functionName).to.equal('CIRCLES')
   })
 
   it('should return INVALID_PARAM for empty address string', async () => {
-    const res = await CIRCLES('', 'trust')
+    const res = await CIRCLES('trust', '')
     expect(res.type).to.equal(ERROR_MESSAGES_FLAG.INVALID_PARAM)
     expect(res.functionName).to.equal('CIRCLES')
   })
@@ -53,14 +53,14 @@ describe('CIRCLES', () => {
     sinon
       .stub(fromEnsNameToAddressUtil.default, 'validateAndGetAddress')
       .resolves('0xe9A6378d8FD4983C2999DB0735f258397E8C2253')
-    const res = await CIRCLES('0xe9A6378d8FD4983C2999DB0735f258397E8C2253', '')
+    const res = await CIRCLES('', '0xe9A6378d8FD4983C2999DB0735f258397E8C2253')
     expect(res.type).to.equal(ERROR_MESSAGES_FLAG.INVALID_PARAM)
     expect(res.functionName).to.equal('CIRCLES')
   })
 
   it('should return INVALID_PARAM when ENS resolution fails', async () => {
     sinon.stub(fromEnsNameToAddressUtil.default, 'validateAndGetAddress').throws(new ValidationError('Invalid address'))
-    const res = await CIRCLES('vitalik.eth', 'trust')
+    const res = await CIRCLES('trust', 'vitalik.eth')
     console.log({ res })
     expect(res.type).to.equal(ERROR_MESSAGES_FLAG.INVALID_PARAM)
     expect(res.functionName).to.equal('CIRCLES')
@@ -71,7 +71,7 @@ describe('CIRCLES', () => {
     sinon.stub(fromEnsNameToAddressUtil.default, 'validateAndGetAddress').resolves(mockAddress)
 
     // SDK will throw an error if it can't connect or if there's a network issue
-    const res = await CIRCLES(mockAddress, 'trust')
+    const res = await CIRCLES('trust', mockAddress)
     // The error should be caught and handled by errorMessageHandler
     expect(res).to.be.an('object')
     expect(res.functionName).to.equal('CIRCLES')
@@ -83,14 +83,14 @@ describe('CIRCLES', () => {
     const mockAddress = '0xe9A6378d8FD4983C2999DB0735f258397E8C2253'
     sinon.stub(fromEnsNameToAddressUtil.default, 'validateAndGetAddress').resolves(mockAddress)
 
-    const res = await CIRCLES(mockAddress, 'transactions')
+    const res = await CIRCLES('transactions', mockAddress)
     expect(res).to.be.an('object')
     expect(res.functionName).to.equal('CIRCLES')
     expect([ERROR_MESSAGES_FLAG.DEFAULT, ERROR_MESSAGES_FLAG.NETWORK_ERROR]).to.include(res.type)
   })
 
   it('should return INVALID_PARAM when address is null', async () => {
-    const res = await CIRCLES(null, 'trust')
+    const res = await CIRCLES('trust', null)
     expect(res.type).to.equal(ERROR_MESSAGES_FLAG.INVALID_PARAM)
     expect(res.functionName).to.equal('CIRCLES')
   })
@@ -99,7 +99,7 @@ describe('CIRCLES', () => {
     sinon
       .stub(fromEnsNameToAddressUtil.default, 'validateAndGetAddress')
       .resolves('0xe9A6378d8FD4983C2999DB0735f258397E8C2253')
-    const res = await CIRCLES('0xe9A6378d8FD4983C2999DB0735f258397E8C2253', null)
+    const res = await CIRCLES('', '0xe9A6378d8FD4983C2999DB0735f258397E8C2253')
     expect(res.type).to.equal(ERROR_MESSAGES_FLAG.INVALID_PARAM)
     expect(res.functionName).to.equal('CIRCLES')
   })
